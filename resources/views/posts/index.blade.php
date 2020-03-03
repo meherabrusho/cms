41cc5bfaeb39659d573347447e7985f706e99296
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2020-03-03 13:56:15 +0600

    Displaying all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,36 @@
         <div class="card-header">Posts</div>
 
         <div class="card-body">
-            
+            <table class="table">
+                <thead>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th></th>
+                    <th></th>
+                </thead>
+
+                <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                            <td>
+                                <img class="img-responsive menu-thumbnails" src="{{ asset('storage/'.$post->image) }}" width="80px" height="60px" alt="">
+                            </td>
+
+                            <td>
+                                {{ $post->title }}
+                            </td>
+
+                            <td>
+                                <a href="" class="btn btn-info btn-sm">Edit</a>
+                            </td>
+
+                            <td>
+                                <a href="" class="btn btn-danger btn-sm">Trash</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
