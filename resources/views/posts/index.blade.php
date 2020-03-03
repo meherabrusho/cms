c0f60effd2dad9ce00800fcf4f45a521184254ca
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2020-03-04 05:40:37 +0600

    Permanently deleting models

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,40 +9,48 @@
         <div class="card-header">Posts</div>
 
         <div class="card-body">
-            <table class="table">
-                <thead>
-                    <th>Image</th>
-                    <th>Title</th>
-                    <th></th>
-                    <th></th>
-                </thead>
+            @if($posts->count() > 0)
+                <table class="table">
+                    <thead>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th></th>
+                        <th></th>
+                    </thead>
 
-                <tbody>
-                    @foreach($posts as $post)
-                        <tr>
-                            <td>
-                                <img class="img-responsive menu-thumbnails" src="{{ asset('storage/'.$post->image) }}" width="80px" height="60px" alt="">
-                            </td>
+                    <tbody>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>
+                                    <img class="img-responsive menu-thumbnails" src="{{ asset('storage/'.$post->image) }}" width="80px" height="60px" alt="">
+                                </td>
 
-                            <td>
-                                {{ $post->title }}
-                            </td>
+                                <td>
+                                    {{ $post->title }}
+                                </td>
 
-                            <td>
-                                <button href="" class="btn btn-info btn-sm">Edit</button>
-                            </td>
+                                @if(!$post->trashed())
+                                    <td>
+                                        <button href="" class="btn btn-info btn-sm">Edit</button>
+                                    </td>
+                                @endif
 
-                            <td>
-                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">Trash</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                <td>
+                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">
+                                            {{ $post->trashed() ? 'Delete' : 'Trash' }}
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h3 class="text-center">No posts yet.</h3>
+            @endif
         </div>
     </div>
 @endsection
