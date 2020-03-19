6e1ebe4586b296a05baf14afb4b35a296a89c4f5
Mohtasin Meherab Sikder <30768212+Rusho-RU@users.noreply.github.com>
2020-03-19 15:41:27 +0600

    Updated Middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,12 @@
                         {{ session()->get('success') }}
                     </div>
                 @endif
+
+                @if(session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session()->get('error') }}
+                    </div>
+                @endif
             </div>
 
             @auth
